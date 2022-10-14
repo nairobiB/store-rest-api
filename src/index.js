df2468a487af13c647f893852106a7415f622757
Nairobi Bodden <breaniebodden@yahoo.com>
2022-10-13 22:42:49 -0600

    fetch todos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const axios = require("axios");
 
 const PORT = 4000;
 
 const app = express();
 
-app.route("/").get((req, res) => {
-  return res.json({
-    message: "Hello World",
-  });
+app.use(morgan("dev"));
+
+app.route("/").get(async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+
+    const { data: todos } = response;
+
+    return res.json({ todos });
+  } catch (error) {
+    console.log(error.message);
+    return res.sendStatus(500);
+  }
 });
 
 app.listen(PORT, () => {
-  console.log("Server started on port", PORT);
+  console.log("Servidor iniciado en el puerto", PORT);
 });
